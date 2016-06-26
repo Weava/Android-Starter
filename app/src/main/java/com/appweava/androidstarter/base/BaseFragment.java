77c96c5470c7816c986fcb2a0fecd3698997c783
Aaron Weaver <waaronl@okstate.edu>
2016-06-26 17:32:53 -0500

    Added basic DI stuff for application

---
+++ app/src/main/java/com/appweava/androidstarter/base/BaseFragment.java
@@ -0,0 +1,13 @@
+package com.appweava.androidstarter.base;
+
+/**
+ * BaseFragment
+ * <p>
+ * Class description here
+ *
+ * @author <a href="aaron@appweava.com">Aaron Weaver</a>
+ * @version 1.0.0
+ * @since 6/26/16
+ */
+public class BaseFragment {
+}
---
+++ app/src/main/java/com/appweava/androidstarter/internal/di/ActivityScope.java
@@ -0,0 +1,13 @@
+package com.appweava.androidstarter.internal.di;
+
+/**
+ * ActivityScope
+ * <p>
+ * Class description here
+ *
+ * @author <a href="aaron@appweava.com">Aaron Weaver</a>
+ * @version 1.0.0
+ * @since 6/26/16
+ */
+public class ActivityScope {
+}
